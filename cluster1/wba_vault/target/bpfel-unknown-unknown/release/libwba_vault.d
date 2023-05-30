9ba9b88cb082d36ccc01e96b623de462b91f14ba
Bryan Cole <brycole@gmail.com>
2023-05-30 14:03:53 +0100

    swap files to WSL ubuntu

--- cluster1/wba_vault/target/bpfel-unknown-unknown/release/libwba_vault.d
+++ cluster1/wba_vault/target/bpfel-unknown-unknown/release/libwba_vault.d
@@ -1,1 +1,1 @@
-/home/bryan/coding/cluster1/wba_vault/target/bpfel-unknown-unknown/release/libwba_vault.rlib: /home/bryan/coding/cluster1/wba_vault/programs/wba_vault/src/lib.rs
+/home/bryan/wba/cluster1/wba_vault/target/bpfel-unknown-unknown/release/libwba_vault.rlib: /home/bryan/wba/cluster1/wba_vault/programs/wba_vault/src/lib.rs
